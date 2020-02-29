7279efaa7d69865ca2e0e1be44f9433034b5bf44
prpr48 <prpr48@naver.com>
2020-02-29 16:41:03 +0900

    basic provider, consumer

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import AppPresenter from "./AppPresenter";
 import Store from "store";
 
-class AppContainer extends Component {
-  render() {
-    return (
-      <Store.Provider>
-        <AppPresenter />
-      </Store.Provider>
-    );
-  }
+function AppContainer() {
+  const state = {
+    message: "Hello"
+  };
+
+  return (
+    <Store.Provider value={state.message}>
+      <AppPresenter />
+    </Store.Provider>
+  );
 }
 
 export default AppContainer;
